053a5b9d0bae47c009997656fdf6e248f87b720d
blaketaravella-lab <blaketaravella@gmail.com>
2026-09-11 15:55:30 -0500

    Limit visual engine migration to new build metadata

--- migrations/0008_visual_family_engine.sql
+++ migrations/0008_visual_family_engine.sql
@@ -1,9 +1,3 @@
-ALTER TABLE prospects ADD COLUMN research_status TEXT NOT NULL DEFAULT 'Not Run';
-ALTER TABLE prospects ADD COLUMN business_vertical TEXT;
-ALTER TABLE prospects ADD COLUMN research_json TEXT;
-ALTER TABLE prospects ADD COLUMN research_error TEXT;
-ALTER TABLE prospects ADD COLUMN researched_at TEXT;
-
 ALTER TABLE prospects ADD COLUMN visual_family TEXT;
 ALTER TABLE prospects ADD COLUMN visual_version TEXT;
 ALTER TABLE prospects ADD COLUMN visual_variant TEXT;
